27e25b0b9b4e73032511d3869738615b32764181
“Kilian-Pichard” <kilianpichard@yahoo.fr>
2021-03-08 11:50:20 +0100

    Nettoyage du code JavaScript

--- js/script.js
+++ js/script.js
@@ -36,20 +36,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
         }
     }))
 
-
-    //let imageCourante = document.getElementsByTagName("img")
     let imageCourante = document.querySelectorAll(".imgS")
-    console.log(imageCourante[0])
-    //window.alert(imageCourante)
-
-    /* imageCourante.addEventListener("click", (event) => {
-         console.log(imageCourante)
-     })*/
-
     imageCourante.forEach(img => img.addEventListener("click", (event) => {
-
         let imgSrc = img.src
-        console.log(imgSrc)
         w = open("", 'image', 'weigth=toolbar=no,scrollbars=no,resizable=yes, width=1000, height=1000')
         w.document.write("<HTML><BODY onblur=\"window.close();\">")
         w.document.write("<center><IMG src=\"" + imgSrc + "\" alt=\"Image agrandie\" height=\"600px\" width=\"auto\" margin=\"auto\"></center>")
