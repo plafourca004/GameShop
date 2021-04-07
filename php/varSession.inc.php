37043038ef0ebe418c8b137f8a60025b32ca9db6
plafourca004 <paul64lafourcade@orange.fr>
2021-04-07 23:12:42 +0200

    Adaptation du panier

--- php/varSession.inc.php
+++ php/varSession.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 //session_start();
-
+/*
 $json = json_decode(file_get_contents("informations.json"), true);
 $users = json_decode(file_get_contents("users.json"), true);
 
@@ -15,7 +15,7 @@ $_SESSION["Xbox"] = $json["jeux"]["Xbox"];
 $_SESSION["categories"] = $json["categories"];
 
 $_SESSION["users"] = $users["users"];
-
+*/
 $_SESSION["logged_in"];
 
 $_SESSION["username"];
